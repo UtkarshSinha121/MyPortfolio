5056ae9fe719329740bf925975ac18f958ba4cb7
UtkarshSinha121 <be20cs115utkarshsinha@gmail.com>
2024-02-26 22:27:16 +0530

    portfolio title and social media links

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -24,16 +24,16 @@ function Home2() {
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Java, HTML, CSS and JavaScript. </b>
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple">React based projects </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Blockchain.
+                  Frontend Design.
                 </b>
               </i>
               <br />
@@ -67,7 +67,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/UtkarshSinha121"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -87,7 +87,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/utkarsh-sinha121/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
